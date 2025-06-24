8244fcda1f4edc1b5893734749a20f2a75fc1a77
root <l.isheja@alustudent.com>
2025-06-24 09:59:37 -0700

    Fix: import add function and print 1 + 2 using correct format and PEP8

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
+a = 1
+b = 2
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
