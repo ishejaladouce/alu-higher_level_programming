56098a0f729a9e1d9e30e6dcc85df8ee07f1aab1
root <l.isheja@alustudent.com>
2025-07-02 08:41:18 -0700

    count keys in  directory

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""5-number_keys
+Return the number of keys in a dictionary.
+"""
+
+
+def number_keys(a_dictionary):
+    """Count the keys in *a_dictionary*.
+
+    Args:
+        a_dictionary (dict): Dictionary to inspect.
+
+    Returns:
+        int: Total number of keys.
+    """
+    return len(a_dictionary)
