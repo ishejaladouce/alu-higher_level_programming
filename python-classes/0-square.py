6a75fe9d32c380d61ca5155cd4a33ca63a77a889
root <l.isheja@alustudent.com>
2025-07-09 06:18:01 -0700

    Define an empty Square class with proper docstrings

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0‑square
+Defines an empty Square class for introductory OOP practice.
+"""
+
+
+class Square:
+    """Represent a square.
+
+    For now the class is intentionally empty (pass) because
+    later tasks will add size, area, etc.
+    """
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This is my README.md file about Classes and Objects
