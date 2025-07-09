86777391cedbf0a80244f8e5886ba6bfa9898d08
root <l.isheja@alustudent.com>
2025-07-09 11:54:06 -0700

    add empty Rectangle class as starter structure

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0-rectangle module
+Defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """Represents a rectangle (no attributes or methods yet)."""
+    pass
--- python-more_classes/0‑rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""0‑rectangle module
-Defines an empty Rectangle class.
-"""
-
-
-class Rectangle:
-    """Represents a rectangle (no attributes yet)."""
-    pass
