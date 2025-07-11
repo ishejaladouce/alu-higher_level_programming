7f7f6c9c410323585b3b06aeff5bc2c20f1da223
root <l.isheja@alustudent.com>
2025-07-11 02:10:30 -0700

    Add lookup function to return list of attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines the lookup function.
+"""
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings containing names of the attributes and methods.
+    """
+    return dir(obj)
