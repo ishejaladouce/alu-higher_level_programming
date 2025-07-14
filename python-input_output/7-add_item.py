9ee69b6ffdc03334fb5e74ffd3f0eb0117f0fc5e
root <l.isheja@alustudent.com>
2025-07-14 05:07:46 -0700

    Delete & re-create file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
 Script that adds all command line arguments to a Python list
-and saves it in a JSON file.
+stored in a JSON file.
 """
 
 import sys
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 filename = "add_item.json"
 
 try:
-    data = load_from_json_file(filename)
+    items = load_from_json_file(filename)
 except FileNotFoundError:
-    data = []
+    items = []
 
-data += sys.argv[1:]
+items.extend(sys.argv[1:])
 
-save_to_json_file(data, filename)
+save_to_json_file(items, filename)
