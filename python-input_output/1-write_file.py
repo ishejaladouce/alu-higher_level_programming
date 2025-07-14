4d464f80d9c094ebe4df7523f78ac8433213ebe5
root <l.isheja@alustudent.com>
2025-07-14 04:33:35 -0700

    Add function to write text to file and return number of characters written

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module that writes a string to a UTF-8 text file and
+returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and returns
+    the number of characters written.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The string to write into the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
