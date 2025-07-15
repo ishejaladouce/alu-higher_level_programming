2dcb741d8d892d25c53c68eaf818d2815bdd0c75
root <l.isheja@alustudent.com>
2025-07-15 11:41:54 -0700

    Convert database, first_table, and name column to utf8mb4 charset with utf8mb4_unicode_ci collation

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,14 @@
+-- 17. Convert database and table to utf8mb4 with utf8mb4_unicode_ci collation
+
+ALTER DATABASE hbtn_0c_0
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
+USE hbtn_0c_0;
+
+ALTER TABLE first_table
+  CONVERT TO CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+  CHANGE name name VARCHAR(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL;
