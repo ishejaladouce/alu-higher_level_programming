c5d1cdfbfdc1bdd1917f3919a3a02b868057d86d
root <l.isheja@alustudent.com>
2025-07-18 03:57:13 -0700

    Move privileges scripts inside SQL_more_queries directory

--- 1-create_user.sql
+++
@@ -1,8 +0,0 @@
--- Create user only if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on all databases and tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Make sure privileges are loaded
-FLUSH PRIVILEGES;
