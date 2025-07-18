4596c2ff209e3a31cbcf48fff9e69dd20c7efdb1
root <l.isheja@alustudent.com>
2025-07-18 04:05:02 -0700

    List privileges of user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
+-- Script to list privileges of user_0d_1 and user_0d_2
+-- SQL keywords are in UPPERCASE as required
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
