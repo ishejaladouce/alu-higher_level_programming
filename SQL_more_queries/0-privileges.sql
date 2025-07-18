a55b1a398351450d782942b09ce9590dda7329b7
root <l.isheja@alustudent.com>
2025-07-18 05:02:46 -0700

    Task 0: Show user privileges for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- Script to list privileges of user_0d_1 and user_0d_2
--- SQL keywords are in UPPERCASE as required
+-- List all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
