c4578bf1899bdd97a2a9296b467ec27c2f236a58
root <l.isheja@alustudent.com>
2025-08-01 12:18:22 -0700

    solve issues

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
-Module for lazy matrix multiplication using NumPy
+Lazy matrix multiplication module using NumPy.
+Contains a function to multiply two matrices using NumPy.
 """
+
 import numpy as np
+
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Multiply two matrices using NumPy
+    Multiplies two matrices using NumPy.
 
     Args:
-        m_a: list of lists of integers/floats
-        m_b: list of lists of integers/floats
+        m_a: First matrix (list of lists of integers or floats)
+        m_b: Second matrix (list of lists of integers or floats)
 
     Returns:
-        A new matrix that is the product of m_a and m_b
+        A NumPy array representing the multiplication of m_a and m_b
+
+    Raises:
+        ValueError: If matrices don't meet requirements or can't be multiplied
     """
     return np.matmul(m_a, m_b)
-
