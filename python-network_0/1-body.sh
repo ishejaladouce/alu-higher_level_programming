a13036b6e75400107afd9807aa4c9d4441b99452
root <l.isheja@alustudent.com>
2025-08-01 02:24:04 -0700

    Fix bugs

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Fetch URL following redirects and show body only if final status code is 200
-response=$(curl -s -L -w "%{http_code}" "$1")
-body=${response::-3}
-code=${response: -3}
-if [ "$code" == "200" ]; then
-    echo -n "$body"
-fi
+# sends GET request to given URL and displays body of response
+curl -sLfG "$1"
