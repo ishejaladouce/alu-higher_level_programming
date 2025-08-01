102ddf0cbc9b8bff5a089d4dc38ce60cad234d6d
root <l.isheja@alustudent.com>
2025-08-01 02:20:14 -0700

    Fix bugs

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# Fetch URL with redirects, show body only if final status is 200
-response=$(curl -s -L -w "%{http_code}" "$1"); body=${response::-3}; code=${response: -3}; [ "$code" == "200" ] && echo "$body"
+# Fetch URL with redirects, show body only if final status is 200, no extra newlines
+response=$(curl -s -L -w "%{http_code}" "$1")
+body=${response::-3}
+code=${response: -3}
+if [ "$code" == "200" ]; then
+    printf "%s" "$body"
+fi
