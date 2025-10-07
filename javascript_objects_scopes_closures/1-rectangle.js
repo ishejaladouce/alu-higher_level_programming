2cc78f97d7174820cd10ed367beef801fca85868
root <l.isheja@alustudent.com>
2025-10-07 07:56:42 -0700

    Add Rectangle class with constructor that sets width and height

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// 1-rectangle.js
+// Defines a Rectangle class with width and height
+
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
